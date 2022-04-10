02a5a07168ac2f6ab22ff215d6f480573537fe38
mpenajoia <penajoia.marco@gmail.com>
2022-04-10 15:46:58 -0700

    useFetch hook working

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import useFetch from "./api/useFetch";
+import Home from "./components/Home";
 
 function App() {
+  const { data, loading, error } = useFetch('https://api.coingecko.com/api/v3/coins/')
+
+  if(loading) return <h1>Loading</h1>
+  if(error) console.log(error)
+  console.log(data)
+
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
+      <Home />
     </div>
   );
 }
